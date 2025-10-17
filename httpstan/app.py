43c436b45d7addac95e990a279412a9e6f24a64f
ahartikainen <hartikainen.ari@gmail.com>
2025-10-17 12:21:55 +0300

    update app.py

--- httpstan/app.py
+++ httpstan/app.py
@@ -4,32 +4,34 @@ Configure the server and schedule startup and shutdown tasks.
 """
 import asyncio
 import logging
+from typing import Any
 
-import aiohttp.web
+from aiohttp import web
 
 import httpstan.routes
 
 try:
-    from uvloop import EventLoopPolicy
-except ImportError:
-    EventLoopPolicy = asyncio.DefaultEventLoopPolicy
+    import uvloop
+except ModuleNotFoundError:
+    uvloop = None
 
 
 logger = logging.getLogger("httpstan")
 
 
-async def _warn_unfinished_operations(app: aiohttp.web.Application) -> None:
+async def _warn_unfinished_operations(app: web.Application) -> None:
     """Warn if tasks (e.g., operations) are unfinished.
 
     Called immediately before tasks are cancelled.
 
     """
-    for name, operation in app["operations"].items():
+    operations: dict[str, dict[str, Any]] = app.get("operations", {})
+    for name, operation in operations.items():
         if not operation["done"]:
-            logger.critical(f"Operation `{name}` cancelled before finishing.")
+            logger.critical(f"Operation `%s` cancelled before finishing.", name)
 
 
-def make_app() -> aiohttp.web.Application:
+def make_app() -> web.Application:
     """Assemble aiohttp Application.
 
     Returns:
@@ -37,9 +39,14 @@ def make_app() -> aiohttp.web.Application:
 
     """
     # default `client_max_size` is 1 MiB. Model `data` is often greater. Set to generous 512 GiB.
-    app = aiohttp.web.Application(client_max_size=512 * 1024**3)
+    app = web.Application(client_max_size=512 * 1024**3)
     httpstan.routes.setup_routes(app)
     # startup and shutdown tasks
     app["operations"] = {}
     app.on_cleanup.append(_warn_unfinished_operations)
+
+    # Enable uvloop globally if it’s installed
+    if uvloop is not None:
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
     return app

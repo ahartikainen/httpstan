80e345178b7cbf1a66f0f43d638e4fdb8fab1606
ahartikainen <hartikainen.ari@gmail.com>
2025-10-18 03:07:01 +0300

    fix fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,12 @@
 import typing
 
 import aiohttp.web
-import pytest
+import pytest_asyncio
 
 import httpstan.app
 
 
-@pytest.fixture
+@pytest_asyncio.fixture
 async def server_host_port_pair(
     unused_tcp_port: int,
 ) -> typing.AsyncGenerator[typing.Tuple[str, int], None]:
@@ -26,7 +26,7 @@ async def server_host_port_pair(
     await runner.cleanup()
 
 
-@pytest.fixture
+@pytest_asyncio.fixture
 async def api_url(server_host_port_pair: typing.Tuple[str, int]) -> str:
     host, port = server_host_port_pair
     return f"http://{host}:{port}/v1"
